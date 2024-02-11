aeb469a5bf14b42c1bc6be6208036cb4684ab049
Artem Kamyshansky <ka@media5.com>
2024-02-11 19:41:45 +0300

    feat: store notify events & run apiclient

---
+++ scheduler/src/client.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from loguru import logger
+import httpx
+
+from config import notify_api
+
+
+class NotifyAPIClient:
+    def __init__(self):
+        self.notify = f"{notify_api.url}publish/"
+
+    async def send_schedule_data(
+        self,
+        newsletter_id: int,
+        template_id: int,
+        group_id: List[int] = None,
+        worker_names: List[str] = None,
+        users_id: List[int] = None,
+    ) -> None:
+
+        data = {
+            "template_id": template_id,
+            "group_id": group_id,
+            "newsletter_id": newsletter_id,
+            "users_id": users_id,
+            "worker_names": worker_names,
+        }
+
+        async with httpx.AsyncClient() as client:
+            logger.error(self.notify)
+            response = await client.post(self.notify, json=data)
+
+        if response.status_code == 200:
+            logger.info("Data sent successfully!")
+        else:
+            logger.info(f"Failed to send data. Status Code: {response.status_code}, Response: {response.text}")
--- scheduler/src/scheduler.py
+++ scheduler/src/scheduler.py
@@ -1,19 +1,44 @@
 import asyncio
+
 from loguru import logger
 import asyncpg
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+
+from client import NotifyAPIClient
 from config import db_params
+
 
 class CronTaskScheduler:
     def __init__(self):
         self.db_params = db_params.dict()
         self.scheduler = AsyncIOScheduler()
+        self.api_client = NotifyAPIClient()
 
-    async def execute_task(self, task_id, task_name):
-        logger.info(f"Executing task {task_name} with ID: {task_id}")
-        await asyncio.sleep(2)
-        logger.info(f"End executing task {task_name} with ID: {task_id}")
+    async def execute_task(self, schedule_id, schedule_name, template_id):
+        logger.info(f"Executing task {schedule_name} with ID: {schedule_id}")
+
+        event_id = await self.create_event(schedule_id)
+
+        await self.api_client.send_schedule_data(
+            template_id=template_id,
+            group_id=[1, 2, 3],
+            newsletter_id=123,
+            users_id=[456, 789],
+            worker_names=["worker1", "worker2"],
+        )
+        logger.info(f"End executing task {schedule_name}. Stored event_id: {event_id}")
+
+    async def create_event(self, schedule_id) -> int | None:
+        logger.info("Inserting event to database...")
+
+        async with asyncpg.create_pool(**self.db_params) as pool:
+            async with pool.acquire() as conn:
+                query = "INSERT INTO events (schedule_id, status) VALUES ($1, $2) RETURNING id"
+                values = (schedule_id, "created")
+                result = await conn.fetchrow(query, *values)
+                logger.info(f"Event inserted with ID: {result['id']}")
+                return result["id"]
 
     async def fetch_cron_tasks(self) -> list:
         logger.info("Fetching cron tasks from database...")
@@ -21,7 +46,7 @@ class CronTaskScheduler:
 
         async with asyncpg.create_pool(**self.db_params) as pool:
             async with pool.acquire() as conn:
-                tasks = await conn.fetch("SELECT id, task_name, cron_expression FROM schedule")
+                tasks = await conn.fetch("SELECT id, name, cron_expression, template_id FROM schedule")
 
         logger.info(f"Found {len(tasks)} cron tasks.")
         return tasks
@@ -30,11 +55,12 @@ class CronTaskScheduler:
         tasks = await self.fetch_cron_tasks()
 
         for task in tasks:
-            task_id, task_name, cron_expression = task
-            logger.info(f"Scheduling task {task_name} with cron expression: {cron_expression}")
-
+            schedule_id, schedule_name, cron_expression, template_id = task
+            logger.info(f"Scheduling task {schedule_name} with cron expression: {cron_expression}")
             trigger = CronTrigger.from_crontab(cron_expression)
-            self.scheduler.add_job(func=self.execute_task, trigger=trigger, args=[task_id, task_name], id=f"task_{task_id}")
+            self.scheduler.add_job(
+                func=self.execute_task, trigger=trigger, args=[schedule_id, schedule_name, template_id]
+            )
 
     async def start_scheduler(self) -> None:
         logger.info("Starting scheduler.")
