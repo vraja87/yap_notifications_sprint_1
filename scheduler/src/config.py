96495495d8e3dc1a57903853a200a8a979a8c25c
Artem Kamyshansky <ka@media5.com>
2024-02-12 20:50:13 +0300

    chore: WIP settings

--- scheduler/src/config.py
+++ scheduler/src/config.py
@@ -1,19 +1,27 @@
-import os
+from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-class DBParams(BaseModel):
-    user: str = os.getenv("NOTIFY_DB_USER")
-    password: str = os.getenv("NOTIFY_DB_PASSWORD")
-    host: str = os.getenv("NOTIFY_DB_HOST")
-    port: str = os.getenv("NOTIFY_DB_PORT")
-    database: str = os.getenv("NOTIFY_DB_NAME")
+env_path = Path("../..") / ".env"
+env_file = env_path
 
 
-class NotifyAPI(BaseModel):
-    url: str = os.getenv("NOTIFY_API")
+class DBParams(BaseSettings):
+    model_config = SettingsConfigDict(env_file=env_file)
+    user: str = Field(env="NOTIFY_DB_USER")
+    password: str = Field(env="NOTIFY_DB_PASSWORD")
+    host: str = Field(env="NOTIFY_DB_HOST")
+    port: str = Field(env="NOTIFY_DB_PORT")
+    database: str = Field(env="NOTIFY_DB_NAME")
 
+
+class NotifyAPI(BaseSettings):
+    model_config = SettingsConfigDict(env_file=env_file)
+    url: str = Field(env="NOTIFY_API")
+
+
+# TODO: any options to work without env file - just with env vars? it workerd on old v1 pydantic...
 
 db_params = DBParams()
 notify_api = NotifyAPI()
