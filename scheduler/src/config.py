6f0dccea7cfe73c871749c9aeb8616eb03b33f6f
Artem Kamyshansky <ka@media5.com>
2024-02-12 20:57:18 +0300

    chore: WIP settings

--- scheduler/src/config.py
+++ scheduler/src/config.py
@@ -9,16 +9,16 @@ env_file = env_path
 
 class DBParams(BaseSettings):
     model_config = SettingsConfigDict(env_file=env_file)
-    user: str = Field(env="NOTIFY_DB_USER")
-    password: str = Field(env="NOTIFY_DB_PASSWORD")
-    host: str = Field(env="NOTIFY_DB_HOST")
-    port: str = Field(env="NOTIFY_DB_PORT")
-    database: str = Field(env="NOTIFY_DB_NAME")
+    user: str = Field("notify", env="NOTIFY_DB_USER")
+    password: str = Field("notify", env="NOTIFY_DB_PASSWORD")
+    host: str = Field("notify_db", env="NOTIFY_DB_HOST")
+    port: str = Field("5432", env="NOTIFY_DB_PORT")
+    database: str = Field("notify", env="NOTIFY_DB_NAME")
 
 
 class NotifyAPI(BaseSettings):
     model_config = SettingsConfigDict(env_file=env_file)
-    url: str = Field(env="NOTIFY_API")
+    url: str = Field("http://api:8000/api/v1/", env="NOTIFY_API")
 
 
 # TODO: any options to work without env file - just with env vars? it workerd on old v1 pydantic...
